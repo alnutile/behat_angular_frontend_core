dbbdde84c9d98881d33948731de64e84ce616e05
alnutile <alfrednutile@gmail.com>
2014-03-20 08:16:06 -0400

    Reports dash getting there need to work on charts

--- app/js/services/reportsServices.js
+++ app/js/services/reportsServices.js
@@ -3,14 +3,14 @@ var reportsServices = angular.module('reportsServices', ['ngResource']);
 reportsServices.factory('ReportsServices', ['$resource',
     function($resource){
         return $resource('/behat_editor_services_v2/sites/:sid/reports/:rid', {}, {
-            query: {method:'GET', params:{sid:'', rid:''}, isArray:true},
+            query: {method:'GET', params:{sid:'', rid:'', meta: true}, isArray:true},
             get:   {method:'GET', params:{sid:'', rid:''}, isArray:true} //got object error if not defining get here
         });
     }]).
     factory('ReportsDash', ['$resource',
         function($resource){
             return $resource('/behat_editor_services_v2/reports/:rid', {}, {
-                query: {method:'GET', params:{rid:''}, isArray:false},
+                query: {method:'GET', params:{rid:''}, isArray:false}
             });
         }]).
     factory('ReportsTestsService', ['$resource',
