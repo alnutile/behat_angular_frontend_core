355b0365776d328d9acd52effa6ee33db77d8f39
alnutile <alfrednutile@gmail.com>
2014-06-17 13:08:48 -0400

    [BPFD-143] Wow this is old

--- app/js/helpers/site_helpers.js
+++ app/js/helpers/site_helpers.js
@@ -5,13 +5,12 @@ sites_helpers.factory('SiteHelpers', [ function(){
             'IE 11': { browser: 'internet explorer', version: '11', platform: 'Windows 8.1' },
             'IE 10': { browser: 'internet explorer', version: '10', platform: 'Windows 8' },
             'IE 9': { browser: 'internet explorer', version: '9', platform: 'Windows 7' },
-            'Firefox 26': { browser: 'firefox', version: '25', platform: 'Linux' },
-            'Firefox 25': { browser: 'firefox', version: '26', platform: 'Linux' },
+            'Firefox - 26': { browser: 'firefox', version: '25', platform: 'Linux' },
+            'Google Chrome - 30': { browser: 'chrome', version: '30', platform: 'Linux' },
             'iPad - 6.1': { browser: 'ipad', version: '6.1', platform: 'Mac 10.8' },
             'iPhone - 6.1': { browser: 'iphone', version: '6.1', platform: 'Mac 10.8' },
-            'Safari 6': { browser: 'safari', version: '6', platform: 'Mac 10.8' },
-            'Google Chrome - 30': { browser: 'chrome', version: '30', platform: 'Linux' },
-            'Android 4.3': { browser: 'android', version: '4.3', platform: 'Linux' }
+            'Safari - 6': { browser: 'safari', version: '6', platform: 'Mac 10.8' },
+            'Android - 4.3': { browser: 'android', version: '4.3', platform: 'Linux' }
         };
 
         return {
